10e3bab34a76881bd933e0562c05b3b268417636
Brian Tran <98brian.tran@gmail.com>
2020-07-15 14:22:47 -0700

    Added cypress end to end testing for appointments

---
+++ cypress/integration/appointment.spec.js
@@ -0,0 +1,66 @@
+describe('Appointment', () => {
+	beforeEach(() => {
+		cy.request("GET", "http://localhost:8001/api/debug/reset");
+
+		cy.visit("/");
+	
+		cy.contains("Monday");
+	});
+
+	it('should book an interview', () => {
+		cy.get("[alt=Add]")
+		.first()
+		.click();
+
+		cy.get('[data-testid=student-name-input]')
+		.type('Lydia Miller-Jones');
+
+		cy.get("[alt='Sylvia Palmer']")
+		.click();
+
+		cy.contains('Save')
+		.click();
+
+		cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+	  cy.contains(".appointment__card--show", "Sylvia Palmer");
+	});
+	
+	it('should edit an interview', () => {
+		// 2. Clicks the edit button for the existing appointment
+		cy.get("[alt=Edit]")
+		.first()
+		.click({force: true});
+
+		// 3. Changes the name and interviewer
+		cy.get('[data-testid=student-name-input]')
+		.clear()
+		.type('Lydia Miller-Jones');
+
+		cy.get("[alt='Tori Malcolm']")
+		.click();
+		// 4. Clicks the save button
+		cy.contains('Save')
+		.click();
+		// 5. Sees the edit to the appointment
+		cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+	  cy.contains(".appointment__card--show", "Tori Malcolm");
+
+
+	});
+
+	it('should cancel an interview', () => {
+		// 2. Clicks the delete button for the existing appointment
+		cy.get("[alt=Delete]")
+		.first()
+		.click({force: true});
+		// 3. Clicks the confirm button
+		cy.contains('Confirm')
+		.click();
+
+		cy.contains('Deleting').should('exist')
+		cy.contains('Deleting').should('not.exist')
+		// 4. Sees that the appointment slot is empty
+		cy.contains(".appointment__card--show", "Archie Cohen")
+    .should("not.exist");
+	});
+});
--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,5 +1,49 @@
 describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
-  });
+	});
+	
+	it('should navigate to Tuesday', () => {
+		cy.visit('/');
+
+		cy.contains("[data-testid=day]", "Tuesday")
+		.click()
+		.should("have.class", "day-list__item--selected");
+	})
+
+	it('should book an interview', () => {
+		cy.visit('/');
+		/*
+		1. Visits the root of our web server
+    2. Clicks on the "Add" button in the second appointment
+    3. Enters their name
+    4. Chooses an interviewer
+    5. Clicks the save button
+    6. Sees the booked appointment
+		*/
+		cy.request('http://localhost:8001/api/debug/reset');
+	})
+	
+	it('should edit an interview', () => {
+		/*
+    1. Visits the root of our web server
+    2. Clicks the edit button for the existing appointment
+    3. Changes the name and interviewer
+    4. Clicks the save button
+    5. Sees the edit to the appointment
+		*/
+		cy.request('http://localhost:8001/api/debug/reset');
+
+	})
+
+	it('should cancel an interview', () => {
+		/*
+    1. Visits the root of our web server
+    2. Clicks the delete button for the existing appointment
+    3. Clicks the confirm button
+    4. Sees that the appointment slot is empty
+		*/
+		cy.request('http://localhost:8001/api/debug/reset');
+	})
+
 });
